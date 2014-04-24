194a729612a15ba824922d44931d8ac03d3d9d50
Smirnov Evgenii <smirnov.ev@vuaro.ru>
2014-04-24 22:39:25 +0400

    push alpha prototype

--- finter/settings.py
+++ finter/settings.py
@@ -1,5 +1,5 @@
 import os
-
+LOGIN_REDIRECT_URL = '/'
 ALLOWED_HOSTS = []
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -24,13 +24,32 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'bootstrap3',
+    'feincms',
+    'feincms.module.page',
+    'feincms.module.medialibrary',
+    'mptt',
+    'south',
+
+    'main', #site base main content
 )
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+)
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -43,6 +62,12 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'finter.urls'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 WSGI_APPLICATION = 'finter.wsgi.application'
--- finter/urls.py
+++ finter/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+
 
 admin.autodiscover()
 
@@ -8,4 +11,13 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'django.contrib.auth.login', name='login')
+)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns+= patterns('',
+    url(r'', include('feincms.urls'))
 )
